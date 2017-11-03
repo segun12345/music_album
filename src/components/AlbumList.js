b0c52e1ead46e18a01c113ec2e216109db595aba
rselvaku <Password1>
2017-11-03 14:49:45 +0000

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {ScrollView, Text, View} from 'react-native';
+import AlbumDetail from './AlbumDetail'
+
+class AlbumList extends Component {
+  state = { albums: [] };
+  
+  componentWillMount() {
+    return fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => response.json())
+      .then((responseJson) => {
+          this.setState({ albums: responseJson });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  generateAlbum() {
+    return this.state.albums.map(album => 
+        <AlbumDetail key={album.title} album={album} />
+    );
+  }
+  
+  render() {    
+      return (
+        <ScrollView>
+          {this.generateAlbum()}
+        </ScrollView>
+    );
+  }
+}
+
+export default AlbumList;
